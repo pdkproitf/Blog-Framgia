7a3e3c2658aa825fd1a3a3c416dd7fb5f2816b88
pdkproitf <phucdaica.pro@gmail.com>
2016-11-02 15:18:27 +0700

    sercure pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, length: {minimum: 8, maximum: 255},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
+  validates :pass, presence: true, length: {minimum: 8}
+
+  before_save { self.email = email.downcase! }
+  has_secure_password
 end
