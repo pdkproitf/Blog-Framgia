f9d5506e61b6952ca6f49cf468c95f4406ba820c
pdkproitf <phucdaica.pro@gmail.com>
2016-11-02 18:42:24 +0700

    sercure

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102074216) do
+ActiveRecord::Schema.define(version: 20161102111923) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email"
-    t.string   "pass"
+    t.string   "name"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
