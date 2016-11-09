30baf261b2a9a9ca560b554c09eaab6227c4c6b9
pdkproitf <phucdaica.pro@gmail.com>
2016-11-09 10:37:39 +0700

    log_out log_in succefull

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,17 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get 'static_pages/about'
-
   get 'static_pages/help'
-
   get 'static_pages/contact'
 
-  resources :users
+  get 'users/new', to: 'users#new', as: 'sign-up'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :users do
+
+  end
   resources :sessions
 end
