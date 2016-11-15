f770575f07ab3c7d89b0deb4e2626b338e68978e
pdkproitf <phucdaica.pro@gmail.com>
2016-11-15 16:31:10 +0700

    test static_page controller success

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+  describe "GET home" do
+    before  {get :home}
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it  "renders the home template" do
+      expect(response).to render_template("home")
+    end
+  end
+
+  describe  "GET about"  do
+    before  { get :about}
+
+    it  "returns http success"  do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the about template" do
+      expect(response).to render_template("about")
+    end
+  end
+
+  describe "GET help" do
+    before  {get :help}
+
+    it  "return http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it  "return the help template" do
+      expect(response).to render_template(:help)
+    end
+  end
+
+  describe  "GET contact" do
+    before  {get :contact}
+
+    it  "return http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it  "return the template contact" do
+      expect(response).to render_template(:contact)
+    end
+  end
+end
