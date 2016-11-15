d892215023896b6c3a19714860725a08754aafc9
pdkproitf <phucdaica.pro@gmail.com>
2016-11-15 08:48:41 +0700

    show follow and unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   validates :name,  length: {minimum: 6}
   validates :email, presence: true, length: {maximum: 255},
@@ -81,6 +86,21 @@ class User < ApplicationRecord
   # See "following users" for the fill implementation
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  # => Follows a user
+  def follow other_user
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # => Unfollows a user
+  def unfollow other_user
+    active_relationships.create(followed_id: other_user.id).destroy
+  end
+
+  # => Return truw if the current user is following the other user
+  def following? other_user
+    following.include?(other_user)
   end
 
   private
