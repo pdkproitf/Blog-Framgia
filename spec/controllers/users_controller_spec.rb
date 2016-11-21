a4aaeabd99fa5538ef39de48c285021fb0b731de
pdkproitf <phucdaica.pro@gmail.com>
2016-11-21 16:44:33 +0700

    test controller user :post create

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe  "should get new"  do
+  let(:user) { create(:user) }
+
+  describe  "GET #new"  do
     before  {get :new}
      it "returns http success" do
       expect(response).to have_http_status(:success)
@@ -10,6 +12,31 @@ RSpec.describe UsersController, type: :controller do
 
     it  "renders the home template" do
       expect(response).to render_template("new")
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      before  {post :create, user: attributes_for(:user)}
+      it 'creates a new user' do
+        expect(User.count).to eq(1)
+      end
+
+      it 'redirects to the "root_path" and set flash' do
+        expect(response).to redirect_to root_path
+        expect(flash[:info]).to match(/Please check your email to activate your account./)
+      end
+    end
+
+    context 'with invalid attributes' do
+      before  {post :create, user: attributes_for(:invalid_user)}
+      it 'creates a new user' do
+        expect(User.count).to eq(0)
+      end
+
+      it 'render to new' do
+        expect(response).to render_template :new
+      end
     end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,4 +5,8 @@ FactoryGirl.define do
     password  "password"
     password_confirmation "password"
   end
+
+  factory :invalid_user, parent: :user do
+    name nil
+  end
 end
